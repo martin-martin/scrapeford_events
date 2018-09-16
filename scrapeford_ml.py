2f95b4f995c725b3e5075f4f1c3665f55ebd2cff
martin-martin <breuss.martin@gmail.com>
2018-09-16 20:16:06 +0200

    add user message

--- scrapeford_ml.py
+++ scrapeford_ml.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# notifications about interesting events for my friend hanging out where I used to
+# notifications about interesting events
+# for my friend who's hanging out where I used to
 import re
 import requests
 import webbrowser
@@ -14,7 +15,8 @@ from pprint import pprint
 pattern = re.compile(r"[Mm]achine|ML|AI|[Ii]ntelligence|[Rr]obot|[Mm]aker")
 # testing with today available events
 #pattern = re.compile(r"[Gg]raduate")
-# define how long the user should have to look at the results before the searchable map opens
+# define how long the user should have to look at the results
+# before the searchable map opens
 wait_time = 5
 # scraping from the Stanford daily events page
 page_url = "https://events.stanford.edu/today"
@@ -29,32 +31,35 @@ all_events = soup.findAll("a", {"class" : "postcard-link"}, href=True)
 ############## ONE FUNCTION ##############
 
 def get_events(all_events):
-	flag = False
-	cool_stuff = list()
+    flag = False
+    cool_stuff = list()
 
-	for event in all_events:
-		if re.search(pattern, event.get_text()):
-			flag = True
-			link = page_stub + event['href']
-			text = event.get_text()
-			cool_stuff.append((link, event))
-	if flag:
-		print("##########################################################################")
-		print("YJay! There's something interesting happening for you today! Check it out:")
-		print("##########################################################################")
-		for event in cool_stuff:
-			# opening the event's detail page
-			webbrowser.open(event[0])
-			# and displaying the info in the console
-			print(event[1])
-		# giving a bit of time to take in the results!
-		time.sleep(wait_time)
-		# opening the searchable map in order to find out how to get there quickly
-		webbrowser.open("https://campus-map.stanford.edu/")
-	# save the resulting list of tuples just for fun
-	return cool_stuff
+    for event in all_events:
+        if re.search(pattern, event.get_text()):
+            flag = True
+            link = page_stub + event['href']
+            text = event.get_text()
+            cool_stuff.append((link, event))
+    if flag:
+        print("##########################################################################")
+        print("YJay! There's something interesting happening for you today! Check it out:")
+        print("##########################################################################")
+        for event in cool_stuff:
+            # opening the event's detail page
+            webbrowser.open(event[0])
+            # and displaying the info in the console
+            print(event[1])
+        # giving a bit of time to take in the results!
+        time.sleep(wait_time)
+        # opening the searchable map in order to find out
+        # how to get there quickly
+        webbrowser.open("https://campus-map.stanford.edu/")
+    # save the resulting list of tuples just for fun
+    return cool_stuff
 
 
 ############## RUN THAT STUFF ##############
 
-get_events(all_events)
+event_list = get_events(all_events)
+if not event_list:  # if there are no matching events
+    print("Take today off. There's nothing going on that interests you.")
